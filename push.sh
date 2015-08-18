849cf2bd39ef334e45aa409e488e48d85d35f00a
Albin <albin.daleby@bytewize.com>
2015-08-18 13:20:49 +0200

    happy homepage, this is a start

--- push.sh
+++ push.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+printf "\n----- Started Push Script -----\n\n"
 while true
 do
 	sleep 0.3
-	rsync -pair --chmod=+rw /f/Hemsida/ albin@daleby.com:/var/www/hemsida/
+	if [ -n "$(rsync -trinl --exclude='*_tmp*' --exclude='config.codekit' --exclude='/node_modules' --exclude='push.sh' --delete-before --chmod=+rw /f/Hemsida/ albin@daleby.com:/var/www/hemsida/)" ]; then
+		date +"%a %d %b %H:%M:%S"
+		printf "\e[32m"
+		rsync -tril --exclude='*_tmp*' --exclude='config.codekit' --exclude='/node_modules' --exclude='push.sh' --delete-before --chmod=+rw /f/Hemsida/ albin@daleby.com:/var/www/hemsida/
+		echo -e "\033[0m"
+	fi
 done
