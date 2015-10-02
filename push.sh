98f3bc1d9dcca23b0144cfeb009128aad0a9a598
Albin <albin.daleby@bytewize.com>
2015-10-02 14:57:35 +0200

    added lots of images and changes

--- push.sh
+++ push.sh
@@ -2,11 +2,11 @@
 printf "\n----- Started Push Script -----\n\n"
 while true
 do
-	sleep 0.3
-	if [ -n "$(rsync -trinl --exclude='*_tmp*' --exclude='config.codekit' --exclude='/node_modules' --exclude='push.sh' --delete-before --chmod=+rw /f/Hemsida/ albin@daleby.com:/var/www/hemsida/)" ]; then
+	sleep 2.0
+	if [ -n "$(rsync -trinl --exclude='*_tmp*' --exclude='config.codekit' --exclude='/node_modules' --exclude='push.sh' --delete-before --chmod=+rw --chown=www-data:www-data /f/Hemsida/ albin@daleby.com:/var/www/hemsida/)" ]; then
 		date +"%a %d %b %H:%M:%S"
 		printf "\e[32m"
-		rsync -tril --exclude='*_tmp*' --exclude='config.codekit' --exclude='/node_modules' --exclude='push.sh' --delete-before --chmod=+rw /f/Hemsida/ albin@daleby.com:/var/www/hemsida/
+		rsync -tril --exclude='*_tmp*' --exclude='config.codekit' --exclude='/node_modules' --exclude='push.sh' --delete-before --chmod=+rw --chown=www-data:www-data /f/Hemsida/ albin@daleby.com:/var/www/hemsida/
 		echo -e "\033[0m"
 	fi
 done
